73fac37c614f8e77baee4bfa1bf5e641c5e574b4
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-28 16:26:17 +0200

    add html components

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,38 @@
 <template>
-  <div >
-      Hello World
+  <div class="container">
+      <h1 class="text-center mt-5">ToDo App</h1>
+      <div class="d-flex mt-5">
+        <input class="form-control w-100 " type="text" name="text">
+        <button class="btn btn-warning" type="button">Add</button>
+      </div>
+  </div>
+  <div class="container">
+    <table class="table table-striped table-hover mt-5 table-bordered ">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"> study js </th>
+          <td>todo</td>
+          <td>
+            <div>
+              <span class="fa fa-pen" ></span>
+            </div>
+          </td>
+          <td>
+            <div>
+              <span class="fa fa-trash" ></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
